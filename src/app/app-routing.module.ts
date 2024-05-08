55d56dafc816f3ad03e0223e2dfc51eab9f6e863
Eliada Ballazhi <eliadaballazhi@gmail.com>
2024-05-08 10:14:37 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstFormComponent } from './Forms/MatLibForms/first-form/first-form.component';
+import { SecondFormComponent } from './Forms/MatLibForms/second-form/second-form.component';
 
 const routes: Routes = [
 {
   path: '',
   component: FirstFormComponent
  
+},
+{
+  path: '/second',
+  component: SecondFormComponent
 }
 ];
 
